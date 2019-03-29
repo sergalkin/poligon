610a0ce72ad29ee0757170bbdad131896a80b070
sergalkin <sa.galkin94@gmail.com>
2019-03-29 17:59:37 +0300

    Lar 5.8 merge

---
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Http\Controllers\Blog\Admin;
+
+use App\Http\Requests\BlogCategoryCreateRequest;
+use App\Http\Requests\BlogCategoryUpdateRequest;
+use App\Models\BlogCategory;
+use Illuminate\Http\Request;
+use App\Repositories\BlogCategoryRepository;
+
+/**
+ * Управление категориями блога
+ *
+ * Class CategoryController
+ * @package App\Http\Controllers\Blog\Admi
+ */
+class CategoryController extends BaseController
+{
+    /**
+     * @var BlogCategoryRepository
+     */
+    private $blogCategoryRepository;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->blogCategoryRepository = app(BlogCategoryRepository::class);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $paginator = $this->blogCategoryRepository->getAllWithPaginate(5);
+
+        return view('blog.admin.categories.index', compact('paginator'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $item = new BlogCategory();
+        $categoryList
+            =  $this->blogCategoryRepository->getForComboBox();
+
+        return view('blog.admin.categories.edit', compact('item', 'categoryList'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(BlogCategoryCreateRequest $request)
+    {
+        $data = $request->input();
+        if (empty($data['slug'])) {
+            $data['slug'] = str_slug($data['title']);
+        }
+
+        $item = (new BlogCategory())->create($data);
+
+        if ($item) {
+            return redirect()->route('blog.admin.categories.edit', [$item->id])
+                ->with(['success' => 'Успешно сохранено']);
+        } else {
+            return back()->withErrors(['msg' => 'Ошибка сохранения'])
+                ->withInput();
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+
+        $item = $this->blogCategoryRepository->getEdit($id);
+        if(empty($item)) {
+            abort(404);
+        }
+
+        $categoryList = $this->blogCategoryRepository->getForComboBox();
+
+        return view('blog.admin.categories.edit', compact('item', 'categoryList'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param BlogCategoryUpdateRequest $request
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(BlogCategoryUpdateRequest $request, $id)
+    {
+
+        $item = $this->blogCategoryRepository->get($id);
+
+        if (empty($item)) {
+            return back()
+                ->withErrors(['msg' => "Запись id=[{$id}] не найдена"])
+                ->withInput();
+        }
+
+        $data = $request->all();
+        $result = $item->fill($data)->save();
+
+        if ($result) {
+            return redirect()
+                ->route('blog.admin.categories.edit', $item->id)
+                ->with(['success' => 'Успешно сохранено']);
+        } else {
+            return back()
+                ->withErrors(['msg' => 'Ошибка соединения'])
+                ->withInput();
+        }
+    }
+
+}
---
+++ app/Repositories/BlogCategoryRepository.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\BlogCategory as Model;
+use Illuminate\Database\Eloquent\Collection;
+
+/**
+ * Class BlogCategoryRepository
+ * @package App\Repositories
+ */
+class BlogCategoryRepository extends CoreRepository
+{
+    /**
+     * @return string
+     */
+    protected function getModelClass()
+    {
+        return Model::class;
+    }
+
+    /**
+     * Получить модель для редактирования в админке
+     *
+     * @param  int $id
+     * @return mixed
+     */
+    public function getEdit($id)
+    {
+        return $this->startConditions()->find($id);
+    }
+
+    /**
+     * Получить список категорий для вывода в выпадающем списке
+     *
+     * @return Collection
+     */
+    public function getForComboBox()
+    {
+        $columns = implode(', ', [
+            'id',
+            'CONCAT (id, ". ", title) AS id_title',
+        ]);
+
+        $result[] = $this->startConditions()->all();
+        $result[] = $this
+            ->startConditions()
+            ->select('blog_categories.*',
+                \DB::raw('CONCAT (id, ". ", title) AS id_title'))
+            ->toBase()
+            ->get();
+
+        $result = $this
+            ->startConditions()
+            ->selectRaw($columns)
+            ->toBase()
+            ->get();
+
+        return $result;
+
+    }
+
+    /**
+     * Получить категории для вывода пагинатором.
+     *
+     * @param int|null $perPage
+     *
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function getAllWithPaginate($perPage = null)
+    {
+        $columns = ['id', 'title', 'parent_id'];
+
+        $result = $this
+            ->startConditions()
+            ->select($columns)
+            ->paginate($perPage);
+
+        return $result;
+    }
+
+}
