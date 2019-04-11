56515bff8336e83ec14d469695d027c16dd611b6
sergalkin <sa.galkin94@gmail.com>
2019-04-11 16:31:54 +0300

    relations

--- app/Models/BlogCategory.php
+++ app/Models/BlogCategory.php
@@ -5,10 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * Class BlogCategory
+ *
+ * @package App\Models
+ *
+ * @property-read BlogCategory $parentCategory
+ * @property-read string $parentTitle
+ */
 class BlogCategory extends Model
 {
     use SoftDeletes;
 
+    /**
+     *  Id of Root category
+     */
+    const ROOT = 1;
+
+    /**
+     * @var array
+     */
     protected $fillable
         = [
             'title',
@@ -16,4 +32,34 @@ class BlogCategory extends Model
             'parent_id',
             'description',
         ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function parentCategory()
+    {
+        return $this->belongsTo(BlogCategory::class, 'parent_id', 'id');
+    }
+
+
+    /**
+     * Accessor
+     * @return string
+     */
+    public function getParentTitleAttribute()
+    {
+        $title = $this->parentCategory->title
+            ?? ($this->isRoot()
+                ? 'Корень'
+                : '????');
+        return $title;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isRoot()
+    {
+        return $this->id === BlogCategory::ROOT;
+    }
 }
